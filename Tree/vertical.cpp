f6d0fc51428acfeb68808fcb5f84068a8486a8e9
Suhani-024 <suhanikapoor024@gmail.com>
2026-07-14 17:04:54 +0530

    Tree

---
+++ Tree/vertical.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+vector<vector<int>> vertical(Node* root){
+    
+    map<int, map<int, multiset<int>>> nodes;
+    queue<pair<Node*,pair<int,int>>> todo;
+    todo.push({root, {0,0}});
+
+    while(!todo.empty()){
+        auto p=todo.front();
+        todo.pop();
+        Node* node=p.first;
+        int x=p.second.first;
+        int y=p.second.second;
+        nodes[x][y].insert(node->data);
+
+        if(node->left) todo.push({node->left,{x-1,y+1}});
+        if(node->right) todo.push({node->right,{x+1,y+1}});
+    }
+
+    vector<vector<int>> ans;
+    for(auto p:nodes){
+        vector<int> col;
+        for(auto q : p.second){
+            col.insert(col.end(),q.second.begin(),q.second.end());
+        }
+        ans.push_back(col);
+    }
+    return ans;
+
+}
